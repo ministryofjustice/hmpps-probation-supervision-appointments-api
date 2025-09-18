5e67634eb522d5145cccb136d7a15b0f6a4d48d6
Amardeep Chimber <amardeep.chimber@digital.justice.gov.uk>
2025-09-18 18:27:14 +0100

    MAN-816 add config

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,10 +11,15 @@ dependencies {
   implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.5.0")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.11")
+  implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+  implementation("org.flywaydb:flyway-core")
   implementation("com.microsoft.graph:microsoft-graph:6.20.0")
   implementation("com.azure:azure-identity:1.13.3")
 
+  runtimeOnly("org.postgresql:postgresql")
+
   testImplementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:1.5.0")
+  testImplementation("com.h2database:h2")
   testImplementation("org.wiremock:wiremock-standalone:3.13.1")
   testImplementation("io.swagger.parser.v3:swagger-parser:2.1.32") {
     exclude(group = "io.swagger.core.v3")

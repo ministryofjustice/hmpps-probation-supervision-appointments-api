63fa0725f8fa852616ec0f1dc4ba615613315ad2
Harsh Vasudev <harsh.vasudev@justice.gov.uk>
2025-09-24 11:54:11 +0100

    MAN-1173 - code scanning vulnerabilities fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,14 +15,27 @@ configurations {
   testImplementation { exclude(group = "org.junit.vintage") }
 }
 
+configurations.all {
+  resolutionStrategy {
+    force("io.netty:netty-codec:4.1.125.Final")
+    force("io.netty:netty-codec-compression:4.2.5.Final")
+    force("org.apache.commons:commons-lang3:3.18.0")
+  }
+}
+
 dependencies {
-  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.5.0")
+  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.7.0")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
-  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.11")
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.13")
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
   implementation("org.flywaydb:flyway-core")
-  implementation("com.microsoft.graph:microsoft-graph:6.20.0")
-  implementation("com.azure:azure-identity:1.13.3")
+  implementation("com.microsoft.graph:microsoft-graph:6.53.0")
+  implementation("org.springframework:spring-core:6.2.11")
+  implementation("org.springframework.security:spring-security-core:6.5.4")
+  implementation("com.azure:azure-identity:1.18.0")
+  implementation("io.netty:netty-codec:4.1.125.Final")
+  implementation("io.netty:netty-codec-compression:4.2.5.Final")
+  implementation("org.apache.commons:commons-lang3:3.18.0")
 
   runtimeOnly("org.flywaydb:flyway-database-postgresql")
   runtimeOnly("org.postgresql:postgresql")
@@ -31,10 +44,10 @@ dependencies {
   developmentOnly("org.wiremock:wiremock-standalone:3.9.1")
   developmentOnly("com.h2database:h2")
 
-  testImplementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:1.5.0")
+  testImplementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:1.7.0")
   testImplementation("com.h2database:h2")
   testImplementation("org.wiremock:wiremock-standalone:3.13.1")
-  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.32") {
+  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.34") {
     exclude(group = "io.swagger.core.v3")
   }
 }

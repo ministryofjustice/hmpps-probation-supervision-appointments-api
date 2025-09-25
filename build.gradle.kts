d64d9c00d132956481843f09f7761a873782ca2b
Harsh Vasudev <harsh.vasudev@justice.gov.uk>
2025-09-25 13:18:01 +0100

    MAN-1173 - Standardised the build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,21 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "9.0.0"
-  id("org.jetbrains.kotlin.plugin.jpa") version "2.2.10"
-  kotlin("plugin.spring") version "2.2.10"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "9.1.1"
+  id("org.jetbrains.kotlin.plugin.jpa") version "2.2.20"
+  kotlin("plugin.spring") version "2.2.20"
   id("idea")
 }
+
+val hmppsStarterVersion = "1.7.0"
+val springCoreVersion = "6.2.11"
+val springSecurityCoreVersion = "6.5.4"
+val azureIdentityVersion = "1.18.0"
+val microsoftGraphVersion = "6.53.0"
+val commonsLangVersion = "3.18.0"
+val nettyCodecVersion = "4.1.125.Final"
+val nettyCompressionVersion = "4.2.5.Final"
+val wiremockVersion = "3.13.1"
+val swaggerParserVersion = "2.1.34"
+val springdocVersion = "2.8.13"
 
 idea {
   module {
@@ -12,42 +24,44 @@ idea {
 }
 
 configurations {
-  testImplementation { exclude(group = "org.junit.vintage") }
+  testImplementation {
+    exclude(group = "org.junit.vintage")
+  }
 }
 
 configurations.all {
   resolutionStrategy {
-    force("io.netty:netty-codec:4.1.125.Final")
-    force("io.netty:netty-codec-compression:4.2.5.Final")
-    force("org.apache.commons:commons-lang3:3.18.0")
+    force("io.netty:netty-codec:$nettyCodecVersion")
+    force("io.netty:netty-codec-compression:$nettyCompressionVersion")
+    force("org.apache.commons:commons-lang3:$commonsLangVersion")
   }
 }
 
 dependencies {
-  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.7.0")
+  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:$hmppsStarterVersion")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
-  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.13")
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:$springdocVersion")
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+  implementation("org.springframework:spring-core:$springCoreVersion")
+  implementation("org.springframework.security:spring-security-core:$springSecurityCoreVersion")
   implementation("org.flywaydb:flyway-core")
-  implementation("com.microsoft.graph:microsoft-graph:6.53.0")
-  implementation("org.springframework:spring-core:6.2.11")
-  implementation("org.springframework.security:spring-security-core:6.5.4")
-  implementation("com.azure:azure-identity:1.18.0")
-  implementation("io.netty:netty-codec:4.1.125.Final")
-  implementation("io.netty:netty-codec-compression:4.2.5.Final")
-  implementation("org.apache.commons:commons-lang3:3.18.0")
+  implementation("com.microsoft.graph:microsoft-graph:$microsoftGraphVersion")
+  implementation("com.azure:azure-identity:$azureIdentityVersion")
+  implementation("org.apache.commons:commons-lang3:$commonsLangVersion")
+  implementation("io.netty:netty-codec:$nettyCodecVersion")
+  implementation("io.netty:netty-codec-compression:$nettyCompressionVersion")
 
   runtimeOnly("org.flywaydb:flyway-database-postgresql")
   runtimeOnly("org.postgresql:postgresql")
 
-  compileOnly("org.wiremock:wiremock-standalone:3.9.1")
-  developmentOnly("org.wiremock:wiremock-standalone:3.9.1")
+  compileOnly("org.wiremock:wiremock-standalone:$wiremockVersion")
+  developmentOnly("org.wiremock:wiremock-standalone:$wiremockVersion")
   developmentOnly("com.h2database:h2")
 
-  testImplementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:1.7.0")
+  testImplementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:$hmppsStarterVersion")
   testImplementation("com.h2database:h2")
-  testImplementation("org.wiremock:wiremock-standalone:3.13.1")
-  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.34") {
+  testImplementation("org.wiremock:wiremock-standalone:$wiremockVersion")
+  testImplementation("io.swagger.parser.v3:swagger-parser:$swaggerParserVersion") {
     exclude(group = "io.swagger.core.v3")
   }
 }
@@ -56,8 +70,6 @@ kotlin {
   jvmToolchain(21)
 }
 
-tasks {
-  withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    compilerOptions.jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21
-  }
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+  compilerOptions.jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21
 }

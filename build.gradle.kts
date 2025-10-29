4af704fb84ebef82ef49316f9538c0f1681370a4
Anand Elangovan <anand.elangovan@digital.justice.gov.uk>
2025-10-29 15:52:09 +0000

    [MAN-1081] Update docker image

--- build.gradle.kts
+++ build.gradle.kts
@@ -47,11 +47,16 @@ dependencies {
 }
 
 kotlin {
-  jvmToolchain(21)
+  jvmToolchain(25)
+}
+
+java {
+  sourceCompatibility = JavaVersion.VERSION_24
+  targetCompatibility = JavaVersion.VERSION_24
 }
 
 tasks {
   withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    compilerOptions.jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21
+    compilerOptions.jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_24
   }
 }

2c515420b0ce1eddb9208804cfc3d78f3a7d2f9d
Amardeep Chimber <amardeep.chimber@digital.justice.gov.uk>
2025-12-03 16:22:10 +0000

    add flipt config to helm

--- src/main/kotlin/uk/gov/justice/digital/hmpps/probationsupervisionappointmentsapi/config/FliptConfig.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/probationsupervisionappointmentsapi/config/FliptConfig.kt
@@ -3,12 +3,10 @@ package uk.gov.justice.digital.hmpps.probationsupervisionappointmentsapi.config
 import io.flipt.client.FliptClient
 import io.flipt.client.models.ClientTokenAuthentication
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-@ConditionalOnProperty("flipt.url")
 class FliptConfig(
   @Value("\${flipt.url}") private val url: String,
   @Value("\${flipt.token}") private val token: String,

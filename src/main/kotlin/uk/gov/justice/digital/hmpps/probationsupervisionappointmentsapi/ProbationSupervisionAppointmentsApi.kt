e2977d692634cb2928a20f0a047a30f4af720394
Anand Elangovan <anand.elangovan@digital.justice.gov.uk>
2026-01-30 15:33:28 +0000

    [MAN-1765] Remove @ConfigurationPropertiesScan

--- src/main/kotlin/uk/gov/justice/digital/hmpps/probationsupervisionappointmentsapi/ProbationSupervisionAppointmentsApi.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/probationsupervisionappointmentsapi/ProbationSupervisionAppointmentsApi.kt
@@ -1,13 +1,11 @@
 package uk.gov.justice.digital.hmpps.probationsupervisionappointmentsapi
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
 @EnableJpaAuditing(modifyOnCreate = true)
-@ConfigurationPropertiesScan
 class ProbationSupervisionAppointmentsApi
 
 fun main(args: Array<String>) {

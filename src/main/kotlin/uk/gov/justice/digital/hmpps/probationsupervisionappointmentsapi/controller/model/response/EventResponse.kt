04085b883f878f2383b338110f18f82f19783050
anandelangovan1 <148061492+anandelangovan1@users.noreply.github.com>
2026-04-17 12:14:20 +0100

    [MAN-2428] Update event response object to include sms response (#93)
    * [MAN-2428] Update event response object to include sms response

--- src/main/kotlin/uk/gov/justice/digital/hmpps/probationsupervisionappointmentsapi/controller/model/response/EventResponse.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/probationsupervisionappointmentsapi/controller/model/response/EventResponse.kt
@@ -1,9 +1,17 @@
 package uk.gov.justice.digital.hmpps.probationsupervisionappointmentsapi.controller.model.response
 
+import java.util.UUID
+
 data class EventResponse(
   val id: String?,
   val subject: String?,
   val startDate: String?,
   val endDate: String?,
   val attendees: List<String>? = emptyList(),
+  val smsResponse: SmsResponse? = null,
+)
+
+data class SmsResponse(
+  val englishNotificationId: UUID? = null,
+  val welshNotificationId: UUID? = null,
 )

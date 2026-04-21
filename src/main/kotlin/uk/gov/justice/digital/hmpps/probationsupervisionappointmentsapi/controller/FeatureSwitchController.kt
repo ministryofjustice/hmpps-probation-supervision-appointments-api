b02ff8f6e1895e651d980ca65c53442b9a1f9830
Anand Elangovan <Anand.Elangovan1@justice.gov.uk>
2026-04-21 11:50:32 +0100

    [MAN-2494] New API for checking if a feature switch is enabled for a user

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/probationsupervisionappointmentsapi/controller/FeatureSwitchController.kt
@@ -0,0 +1,36 @@
+package uk.gov.justice.digital.hmpps.probationsupervisionappointmentsapi.controller
+
+import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import uk.gov.justice.digital.hmpps.probationsupervisionappointmentsapi.controller.model.response.FeatureSwitchResponse
+import uk.gov.justice.digital.hmpps.probationsupervisionappointmentsapi.service.FeatureFlagsService
+import uk.gov.justice.digital.hmpps.probationsupervisionappointmentsapi.service.SmsService
+
+@RestController
+@Tag(name = "Feature switch", description = "Feature switch service to check if a feature is enabled/disabled")
+@PreAuthorize("hasRole('ROLE_PROBATION_API__PROBATION_SUPERVISION_APPOINTMENTS__EVENTS')")
+@RequestMapping("/feature-switch")
+class FeatureSwitchController(val featureSwitchService: FeatureFlagsService) {
+
+  @RequestMapping(
+    method = [RequestMethod.GET],
+    value = ["/isFeatureEnabledForUser"],
+    produces = ["application/json"],
+  )
+  fun isUserEnabledForAFeature(
+    @Parameter(description = "The persons email address") @RequestParam(
+      value = "email",
+      required = true,
+    ) email: String,
+    @Parameter(description = "Name of the feature switch") @RequestParam(
+      value = "featureSwitchName",
+      required = true,
+    ) featureSwitchName: String
+  ) = FeatureSwitchResponse(featureSwitchService.isEnabledForUser(featureSwitchName, email))
+
+}

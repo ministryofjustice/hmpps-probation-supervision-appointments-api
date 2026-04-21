93797a8000cd2d4472ed41ff89678d7d55a7a72a
anandelangovan1 <148061492+anandelangovan1@users.noreply.github.com>
2026-04-21 12:09:07 +0100

    MAN-2494 New API for checking if a feature switch is enabled for a user (#99)
    * [MAN-2494] New API for checking if a feature switch is enabled for a user

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/probationsupervisionappointmentsapi/controller/model/response/FeatureSwitchResponse.kt
@@ -0,0 +1,5 @@
+package uk.gov.justice.digital.hmpps.probationsupervisionappointmentsapi.controller.model.response
+
+data class FeatureSwitchResponse(
+  val enabled: Boolean,
+)
---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/probationsupervisionappointmentsapi/integration/api/FeatureSwitchControllerIntegrationTest.kt
@@ -0,0 +1,50 @@
+package uk.gov.justice.digital.hmpps.probationsupervisionappointmentsapi.integration.api
+
+import org.junit.jupiter.api.Test
+import org.mockito.kotlin.whenever
+import org.springframework.test.context.bean.override.mockito.MockitoBean
+import uk.gov.justice.digital.hmpps.probationsupervisionappointmentsapi.integration.IntegrationTestBase
+import uk.gov.justice.digital.hmpps.probationsupervisionappointmentsapi.service.FeatureFlagsService
+
+class FeatureSwitchControllerIntegrationTest : IntegrationTestBase() {
+
+  @MockitoBean
+  lateinit var featureFlagsService: FeatureFlagsService
+
+  @Test
+  fun `unauthorized status returned`() {
+    webTestClient.post().uri("/feature-switch/isFeatureEnabledForUser")
+      .exchange()
+      .expectStatus().isUnauthorized
+  }
+
+  @Test
+  fun `successful response for a given user when feature switch is enabled for user`() {
+    whenever { featureFlagsService.isEnabledForUser("featureSwitchName", "test@test.com") }.thenReturn(true)
+
+    webTestClient.get().uri {
+      it.path("/feature-switch/isFeatureEnabledForUser")
+        .queryParam("email", "test@test.com")
+        .queryParam("featureSwitchName", "featureSwitchName")
+        .build()
+    }
+      .headers(setAuthorisation())
+      .exchange()
+      .expectStatus().isOk
+      .expectBody().jsonPath("$.enabled").isEqualTo(true)
+  }
+
+  @Test
+  fun `when a feature switch is not enabled for a user, false is returned`() {
+    webTestClient.get().uri {
+      it.path("/feature-switch/isFeatureEnabledForUser")
+        .queryParam("email", "test@test.com")
+        .queryParam("featureSwitchName", "featureSwitchName")
+        .build()
+    }
+      .headers(setAuthorisation())
+      .exchange()
+      .expectStatus().isOk
+      .expectBody().jsonPath("$.enabled").isEqualTo(false)
+  }
+}

9961f2a3f5d78a1427aec2e56eca911ccb01b435
Anand Elangovan <Anand.Elangovan1@justice.gov.uk>
2026-05-01 12:23:35 +0100

    [MAN-2589] Add sentry errors to exceptions

--- src/main/kotlin/uk/gov/justice/digital/hmpps/probationsupervisionappointmentsapi/service/FeatureFlagsService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/probationsupervisionappointmentsapi/service/FeatureFlagsService.kt
@@ -1,6 +1,7 @@
 package uk.gov.justice.digital.hmpps.probationsupervisionappointmentsapi.service
 
 import io.flipt.client.FliptClient
+import io.sentry.Sentry
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
@@ -13,15 +14,6 @@ class FeatureFlagsService(
     private val log: Logger = LoggerFactory.getLogger(this::class.java)
   }
 
-  fun isEnabled(key: String) = try {
-    client
-      .evaluateBoolean(key, key, emptyMap<String, String>())
-      .isEnabled
-  } catch (e: Exception) {
-    log.warn("Error retrieving feature flag '$key', defaulting to disabled", e)
-    false
-  }
-
   fun isEnabledForUser(key: String, emailAddress: String) = try {
     client.evaluateBoolean(
       key,
@@ -30,6 +22,7 @@ class FeatureFlagsService(
     ).isEnabled
   } catch (e: Exception) {
     log.warn("Error retrieving feature flag '$key', defaulting to disabled", e)
+    Sentry.captureException(e)
     false
   }
 }

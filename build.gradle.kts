e8fd31b1b871c6bd50665ada299b1ec02a6aaae2
achimber-moj <161360519+achimber-moj@users.noreply.github.com>
2026-08-14 09:03:46 +0100

    Mtrsb 125 update supression file (#156)
    * MTRSB-125: update supression file
    
    * MTRSB-125: update supression file

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,7 @@ val microsoftGraphVersion = "6.67.0"
 val wiremockVersion = "3.13.2"
 val swaggerParserVersion = "2.1.46"
 val springdocVersion = "3.1.0"
+val httpclient5Version = "5.6.3"
 val sqsVersion = "7.4.0"
 val postgresqlVersion = "42.7.13"
 
@@ -24,12 +25,17 @@ idea {
   }
 }
 
+// Overrides Spring Boot's managed httpclient5 version (see spring-boot-dependencies BOM) to
+// pick up the fix for CVE-2026-64607 (classic transport fails to release the underlying
+// connection when it encounters an invalid/unsupported Content-Encoding header).
+extra["httpclient5.version"] = httpclient5Version
+
 configurations {
   testImplementation { exclude(group = "org.junit.vintage") }
 }
 
 dependencyCheck {
-  suppressionFiles.add("azure-dependency-check-suppress.xml")
+  suppressionFiles.add("owasp-suppressions.xml")
 }
 
 dependencies {
